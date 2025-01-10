f04749847cf4e524cd2c8b096d18818111bf2f71
Aayush <aayushk4980@gmail.com>
2025-01-10 13:24:51 +0530

    Signup route

---
+++ src/pages/api/auth/signup.ts
@@ -0,0 +1,32 @@
+import connectMongoDb from "@/lib/db";
+import User from "@/models/User";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ success: false, message: "Method not allowed" });
+    };
+
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ success: false, message: "Missing required fields" });
+    }
+
+    try {
+        await connectMongoDb();
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: "User already exists" });
+        }
+
+        const newUser = new User({ name, email, password });
+
+        await newUser.save();
+        res.status(201).json({ success: true, message: "User created successfully" });
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal server error", error });
+    }
+}
