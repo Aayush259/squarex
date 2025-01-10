512ecb3af396652a8a75ad3a08ec3ea3abefeb4f
Aayush <aayushk4980@gmail.com>
2025-01-10 13:24:43 +0530

    Setup next-auth

---
+++ src/pages/api/auth/[nextauth].ts
@@ -0,0 +1,43 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import connectMongoDb from '@/lib/db';
+import User from '@/models/User';
+
+export default NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            authorize: async (credentials) => {
+                await connectMongoDb();
+                const user = await User.findOne({ email: credentials?.email });
+                if (user && await user.comparePassword(credentials?.password!)) {
+                    return { id: (user._id as string).toString(), name: user.name, email: user.email };
+                } else {
+                    return null;
+                }
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt",
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (token && session.user) {
+                session.user.id = token.id as string;
+            }
+            return session;
+        },
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+});
