5a7b638a9978166fb3ec2c56a1f5e87f6fbcb89f
Aayush <aayushk4980@gmail.com>
2025-01-12 16:19:15 +0530

    Update layout and globals.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Heebo } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,11 @@ const geistMono = Geist_Mono({
     subsets: ["latin"],
 });
 
+const heebo = Heebo({
+    variable: "--font-heebo",
+    subsets: ["latin"],
+});
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -19,7 +24,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${geistSans.variable} ${geistMono.variable} ${heebo.variable} antialiased`}
             >
                 {children}
             </body>
