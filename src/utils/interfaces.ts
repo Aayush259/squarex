804d4d7b362cdfcf0ebdf8e127c29ccc4322632b
Aayush <aayushk4980@gmail.com>
2025-01-13 21:53:49 +0530

    Updated interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -8,3 +8,24 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
     children: React.ReactNode;
     className?: string;
 };
+
+export interface ITemplates {
+    name: TemplateType;
+    description: string;
+    image: string;
+}
+
+// Store Interfaces
+export interface UserState {
+    id: string;
+    name: string;
+    email: string;
+};
+
+export type TemplateType = 'basic1template' | null;
+
+export interface TemplateState {
+    mode: 'checking' | 'editing' | 'reviewing' | 'done' | null;
+    selectedTemplate: TemplateType;
+    templateData: Record<string, any>;
+};
