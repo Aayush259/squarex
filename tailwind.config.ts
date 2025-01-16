6895739b17a29bda902a48ba26e1066d9d36de90
Aayush <aayushk4980@gmail.com>
2025-01-17 01:13:04 +0530

    Added animations and keyframes for skills section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,25 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        "scroll1": {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        "scroll2": {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-200%)" },
+        },
+        "spin2": {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        }
+      },
+      animation: {
+        "scroll1": "scroll1 30s linear infinite forwards",
+        "scroll2": "scroll2 30s linear 15s infinite forwards",
+        "spin2": "spin2 3s linear infinite"
+      }
     },
   },
   plugins: [],
