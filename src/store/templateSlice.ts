e2a72ec9c5881642061ec21130bc7b326613e686
Aayush <aayushk4980@gmail.com>
2025-01-17 01:13:27 +0530

    Updated templateSlice

--- src/store/templateSlice.ts
+++ src/store/templateSlice.ts
@@ -1,10 +1,10 @@
-import { TemplateState, TemplateType } from '@/utils/interfaces';
+import { TemplateDataMap, TemplateState, TemplateType } from '@/utils/interfaces';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 const initialState: TemplateState = {
     mode: null,
     selectedTemplate: null,
-    templateData: {},
+    templateData: null,
 };
 
 const templateSlice = createSlice({
@@ -17,7 +17,9 @@ const templateSlice = createSlice({
         setSelectedTemplate: (state, action: PayloadAction<TemplateType>) => {
             state.selectedTemplate = action.payload;
         },
-        setTemplateData: (state, action: PayloadAction<Record<string, any>>) => {
+        setTemplateData: (state, action: PayloadAction<{
+            [K in Exclude<TemplateType, null>]: K extends keyof TemplateDataMap ? TemplateDataMap[K] : never;
+        } | null>) => {
             state.templateData = action.payload;
         },
         resetTemplate: (state) => {
