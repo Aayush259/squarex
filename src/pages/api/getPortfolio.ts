a841ee29fce06005a919c5bd73ca28d153a66db9
Aayush <aayushk4980@gmail.com>
2025-01-19 15:21:28 +0530

    API route to get portfolio

---
+++ src/pages/api/getPortfolio.ts
@@ -0,0 +1,32 @@
+import connectMongoDb from "@/lib/db";
+import { NextApiRequest, NextApiResponse } from "next";
+import mongoose from "mongoose";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== "POST") {
+        return res.status(405).json({ success: false, message: "Method not allowed" });
+    }
+
+    const { templateName, slug } = req.body;
+
+    if (!templateName || !slug) {
+        return res.status(400).json({ success: false, message: "Missing required fields" });
+    }
+
+    try {
+        await connectMongoDb();
+
+        const template = await mongoose.connection.collection(templateName).findOne({ user_id: slug });
+
+        if (template) {
+            // Update the existing template
+            res.status(200).json({ success: true, message: "Portfolio", data: template });
+        } else {
+            
+            res.status(404).json({ success: false, message: "Portfolio not found" });
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ success: false, message: "Internal server error", error });
+    }
+}
