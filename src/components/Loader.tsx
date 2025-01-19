280e17ef218542194572340a6c4e6610a81d3783
Aayush <aayushk4980@gmail.com>
2025-01-19 22:42:39 +0530

    Added Loaders

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,26 @@
 
+export const Loader = () => {
+
+    return (
+        <div className="flex flex-row items-center justify-center mx-auto w-fit">
+            {
+                ['animate-loading1', 'animate-loading2', 'animate-loading3'].map(animation => (
+                    <div
+                        key={animation}
+                        className={`w-3 h-3 m-1 rounded-full bg-white opacity-0 ${animation}`}
+                    />
+                ))
+            }
+        </div>
+    );
+};
+
 export const CreatingPortfolioSpinner = () => {
 
     return (
         <div className="fixed w-screen h-screen top-0 left-0 z-[100] bg-black/90 flex items-center justify-center">
             <div>
-                <div className="animate-spin rounded-full h-12 w-12 mx-auto border-t-2 border-b-2 border-white" />
+                <Loader />
                 <p className="text-white text-lg text-center mt-8">
                     {"Setting up your portfolio..."}
                 </p>
@@ -14,4 +30,13 @@ export const CreatingPortfolioSpinner = () => {
             </div>
         </div>
     );
+};
+
+export const FullPageLoader = () => {
+
+    return (
+        <div className="fixed w-screen h-screen top-0 left-0 z-[100] bg-black/90 flex items-center justify-center">
+            <Loader />
+        </div>
+    )
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,12 +24,20 @@ export default {
         "spin2": {
           "0%": { transform: "rotate(0deg)" },
           "100%": { transform: "rotate(360deg)" },
-        }
+        },
+        "loading": {
+          "0%": { transform: "scale(1)", opacity: "1" },
+          "50%": { transform: "scale(0)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
+        },
       },
       animation: {
         "scroll1": "scroll1 30s linear infinite forwards",
         "scroll2": "scroll2 30s linear 15s infinite forwards",
-        "spin2": "spin2 3s linear infinite"
+        "spin2": "spin2 3s linear infinite",
+        "loading1": "loading 1s infinite",
+        "loading2": "loading 1s infinite 0.2s",
+        "loading3": "loading 1s infinite 0.4s",
       }
     },
   },
