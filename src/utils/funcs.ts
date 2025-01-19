eb55df9971b89ddc08802a0b5990a07c3448f703
Aayush <aayushk4980@gmail.com>
2025-01-19 15:20:52 +0530

    Added function to get image file base 64 from its object url

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -15,3 +15,24 @@ export const restoreCursorPosition = (element: HTMLSpanElement | null, cursorPos
         }
     });
 };
+
+export const getImageFileBase64 = async (imageObjectUrl: string): Promise<string> => {
+    if (!imageObjectUrl.startsWith("blob:")) return imageObjectUrl;
+
+    const blob = await fetch(imageObjectUrl).then(res => res.blob());
+    const mimeToExtension: { [key: string]: string } = {
+        "image/png": "png",
+        "image/jpeg": "jpg",
+        "image/jpg": "jpg",
+        "image/webp": "webp",
+    }
+    const extension = mimeToExtension[blob.type] || "png";
+    const file = new File([blob], `image.${extension}`, { type: blob.type });
+
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result as string);
+        reader.onerror = (error) => reject(error);
+        reader.readAsDataURL(file);
+    });
+};
