f62f46928496fc38b36564930d92f52e5b704838
Aayush <aayushk4980@gmail.com>
2025-01-19 22:47:45 +0530

    Update useFetchApi.ts

--- src/hooks/useFetchApi.ts
+++ src/hooks/useFetchApi.ts
@@ -5,12 +5,17 @@ export default async function useFetchApi<Data>(url: string, options?: RequestIn
         const response = await fetch(url, options);
 
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            if (response.status === 404) {
+                return { data: null, error: "NOT_FOUND" };
+            } else {
+                throw new Error('Network response was not ok');
+            }
         }
 
         const result = await response.json();
+        console.log(result);
         if (!result.success) {
-            return { data: null, error: result.message };
+            return { data: null, error: result.message as string };
         }
         return { data: result?.data, error: null };
     } catch (error) {
