e926b3e0d7f6d46ea93dae1f6ebaf5e186394ac6
Aayush <aayushk4980@gmail.com>
2025-01-20 22:40:51 +0530

    Done sending message

--- src/components/basics/basic1/Contact.tsx
+++ src/components/basics/basic1/Contact.tsx
@@ -1,13 +1,44 @@
 "use client";
+import { sendMessage } from "@/apis/contact";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { selectTemplateMode } from "@/store/templateSlice";
 import { IDs } from "@/utils/helper";
+import { useParams } from "next/navigation";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Basic1Contact = () => {
 
+    const params = useParams();
+    const slug = params?.slug;
+
     const templateMode = useSelector(selectTemplateMode);
+
+    const [sending, setSending] = useState(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (templateMode !== "done" || !templateMode || !slug) return;
+
+        setSending(true);
+        const { data, error } = await sendMessage(slug as string, formData.name, formData.email, formData.message, "basic1template");
+
+        if (data) {
+            setFormData({
+                name: "",
+                email: "",
+                message: "",
+            })
+        }
+
+        setSending(false);
+    }
 
     if (templateMode === "editing") return;
 
@@ -16,15 +47,33 @@ const Basic1Contact = () => {
             <h2 className="text-xl md:text-2xl font-semibold">
                 {"Connect with me"}
             </h2>
-            <form className="py-4">
-                <Input label="Name" className="!bg-white !border-transparent focus:!border-[var(--primary)]" />
-                <Input label="Email" className="!bg-white !border-transparent focus:!border-[var(--primary)]" />
+            <form className="py-4" onSubmit={handleSubmit}>
+                <Input
+                    label="Name"
+                    className="!bg-white !border-transparent focus:!border-[var(--primary)]"
+                    value={formData.name}
+                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                />
+
+                <Input
+                    label="Email"
+                    className="!bg-white !border-transparent focus:!border-[var(--primary)]"
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
 
                 <span>Message:</span>
-                <textarea name="message" id="message" rows={5} className="w-full rounded-md bg-white !border-transparent border focus:!border-[var(--primary)] mt-1 p-2 outline-none" />
+                <textarea
+                    name="message"
+                    id="message"
+                    rows={5}
+                    className="w-full rounded-md bg-white !border-transparent border focus:!border-[var(--primary)] mt-1 p-2 outline-none"
+                    value={formData.message}
+                    onChange={(e) => setFormData({ ...formData, message: e.target.value })}
+                />
 
                 <Button className="block w-full border border-[var(--primary)] my-4 !rounded-lg before:!rounded-lg">
-                    {"Send"}
+                    {sending ? "Sending..." : "Send"}
                 </Button>
             </form>
         </section>
