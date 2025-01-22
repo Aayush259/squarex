17d179f0b3f9107fb3bfcd609ccbcb16dda39d32
Aayush <aayushk4980@gmail.com>
2025-01-22 22:58:37 +0530

    Update layouts

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -1,26 +1,15 @@
 "use client";
-import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { setUser } from "@/store/userSlice";
 import { setMode } from "@/store/templateSlice";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+
     const dispatch = useDispatch();
-    const { data: session } = useSession();
 
     useEffect(() => {
-        if (session?.user) {
-            if (session.user.id) {
-                dispatch(setUser({
-                    id: session.user.id,
-                    name: session.user.name || '',
-                    email: session.user.email || '',
-                }));
-            }
-        }
         dispatch(setMode("done"));
-    }, [session, dispatch]);
+    }, [dispatch]);
 
     return children;
 };
--- src/app/template/layout.tsx
+++ src/app/template/layout.tsx
@@ -1,25 +1,27 @@
 "use client";
 import { useEffect, useState } from "react";
-import { selectTemplateMode, setMode, setTemplateData } from "@/store/templateSlice";
-import { useSession } from "next-auth/react";
+import { setMode, setTemplateData } from "@/store/templateSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { usePathname } from "next/navigation";
 import { FullPageLoader } from "@/components/Loader";
 import { getPortfolioWithBasic1Template } from "@/apis/getPortfolio";
+import { selectUser } from "@/store/userSlice";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 
     const dispatch = useDispatch();
     const pathname = usePathname();
-    const { data: session } = useSession();
 
-    const templateMode = useSelector(selectTemplateMode);
+    const user = useSelector(selectUser);
+
+    const [initializingTemplate, setInitializingTemplate] = useState<boolean>(true);
 
     const initializeTemplate = async () => {
-        if (!pathname || !session?.user?.id) return;
+        setInitializingTemplate(true);
+        if (!pathname || !user?.id) return;
 
         if (pathname.includes("basic1template")) {
-            const { data, error } = await getPortfolioWithBasic1Template(session.user.id);
+            const { data, error } = await getPortfolioWithBasic1Template(user.id);
 
             if (data) {
                 dispatch(setTemplateData({
@@ -28,14 +30,15 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             }
         }
 
+        setInitializingTemplate(false);
         dispatch(setMode("editing"));
     };
 
     useEffect(() => {
         initializeTemplate();
-    }, [pathname, session]);
+    }, [pathname, user]);
 
-    if (!session?.user?.id || !templateMode) return <FullPageLoader />;
+    if (initializingTemplate) return <FullPageLoader />;
 
     return children;
 };
