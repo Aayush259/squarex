0e8ebf2f14c723bb57dbbcda3e054365c923c3c7
Aayush <aayushk4980@gmail.com>
2025-01-26 22:49:59 +0530

    Basic2template page

---
+++ src/app/template/basic2template/page.tsx
@@ -0,0 +1,13 @@
+import Basic2 from "@/components/templates/basic/Basic2";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "B1 Template - SquareX",
+    description: "",
+};
+
+export default function Basic1Template() {
+    return (
+        <Basic2 />
+    );
+};
