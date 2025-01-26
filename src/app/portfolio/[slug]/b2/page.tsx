2c0b53aed9dfeb4953610fc87772fc653f1c7c4f
Aayush <aayushk4980@gmail.com>
2025-01-27 02:17:24 +0530

    Portfolio page for b2

---
+++ src/app/portfolio/[slug]/b2/page.tsx
@@ -0,0 +1,14 @@
+import Basic2 from "@/components/templates/basic/Basic2";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "B2 Portfolio - SquareX",
+    description: "",
+};
+
+export default function Basic2TemplatePortfolio() {
+    
+    return (
+        <Basic2 />
+    );
+};
