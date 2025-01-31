d95153df7dca06a78d6e70fe5081ce3e23f995fd
Aayush <aayushk4980@gmail.com>
2025-01-31 18:31:27 +0530

    Page for intermediate1templaet

---
+++ src/app/template/intermediate1template/page.tsx
@@ -0,0 +1,14 @@
+import Basic1 from "@/components/templates/basic/Basic1";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "I1 Template - SquareX",
+    description: "",
+};
+
+export default function Intermediate1Template() {
+    return (
+        <></>
+        // <Basic1 />
+    );
+};
