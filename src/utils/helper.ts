c9a5c259bcc062a90147fdf9b982e17146787eb5
Aayush259 <aayushk4980@gmail.com>
2025-03-08 21:07:45 +0530

    Update helper.ts

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -2,6 +2,7 @@
 export const templateNames = {
     Basic1Template: "Basic1Template",
     Basic2Template: "Basic2Template",
+    Intermediate1Template: "Intermediate1Template",
 }
 
 export const IDs = {
