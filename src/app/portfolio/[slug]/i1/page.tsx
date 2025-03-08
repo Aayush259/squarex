fae00e53a476a3e2c9a1c3d0b903985f121d3f6e
Aayush259 <aayushk4980@gmail.com>
2025-03-08 21:08:47 +0530

    Portfolio page for intermediate1template

---
+++ src/app/portfolio/[slug]/i1/page.tsx
@@ -0,0 +1,14 @@
+import Intermediate1 from "@/components/templates/intermediate/Intermediate1";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "I1 Portfolio - SquareX",
+    description: "",
+};
+
+export default function Intermediate1TemplatePortfolio() {
+    
+    return (
+        <Intermediate1 />
+    );
+};
