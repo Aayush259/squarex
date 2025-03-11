d2e89c08c7fe242a0834e36c52f1a2048ec2597a
Aayush259 <aayushk4980@gmail.com>
2025-03-12 01:06:15 +0530

    Updated Contact model

--- src/models/Contact.ts
+++ src/models/Contact.ts
@@ -7,12 +7,16 @@ interface IContact extends Document {
         name: string;
         email: string;
         message: string;
-    }
+    };
+    visited: boolean;
+    createdAt: Date;
 }
 
 const contactSchema = new Schema<IContact>({
     user_id: { type: String, required: true },
     data: { type: Object, required: true },
+    visited: { type: Boolean, required: true, default: false },
+    createdAt: { type: Date, required: true, default: Date.now },
 });
 
 const Contact: Model<IContact> = mongoose.models.Contact || mongoose.model<IContact>("Contact", contactSchema);
