8b071d4c9a3612a9c6ba11bbbf7dc5bb4f3b069e
Aayush259 <aayushk4980@gmail.com>
2025-03-17 22:36:40 +0530

    Updated UserTemplate model to store engagements

--- src/models/UserTemplates.ts
+++ src/models/UserTemplates.ts
@@ -4,11 +4,21 @@ import mongoose, { Document, Model, Schema } from "mongoose";
 interface IUserTemplate extends Document {
     user_id: string;
     templateNames: TemplateType[];
+    engagement: {
+        socialClicks: number;
+        projectClicks: number;
+        timeSpent: number;
+    }
 }
 
 const userTemplatesSchema = new Schema<IUserTemplate>({
     user_id: { type: String, required: true },
     templateNames: { type: Array, default: [] },
+    engagement: {
+        socialClicks: { type: Number, default: 0 },
+        projectClicks: { type: Number, default: 0 },
+        timeSpent: { type: Number, default: 0 },
+    }
 });
 
 const UserTemplates: Model<IUserTemplate> = mongoose.models.UserTemplates || mongoose.model<IUserTemplate>("UserTemplates", userTemplatesSchema);
