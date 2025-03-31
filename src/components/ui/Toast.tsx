5e694c9a1766e75b19b48e7d1c51bcf87ab7cd05
Aayush259 <aayushk4980@gmail.com>
2025-03-31 16:06:36 +0530

    Add Toast component for displaying temporary messages with auto-dismiss functionality

---
+++ src/components/ui/Toast.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { IoClose } from "react-icons/io5";
+
+const Toast: React.FC<{
+    message: string;
+    onClose: () => void;
+    success: boolean;
+}> = ({ message, onClose, success }) => {
+
+    useEffect(() => {
+        setTimeout(() => {
+            onClose();
+        }, 4000);
+    }, [])
+
+    return (
+        <div className="bg-slate-800 max-w-[85vw] w-[400px] mx-auto md:mx-0 p-3 md:py-4 md:px-8 text-center text-white flex justify-between items-center rounded-xl m-2 md:text-xl overflow-ellipsis animate-in relative z-[1000] overflow-hidden gap-2">
+            <div
+                className={`absolute h-1 left-0 bottom-0 ${success ? 'bg-green-500' : 'bg-red-500'} animate-toast`}
+            />
+            <div className="flex items-center justify-start text-left gap-2 flex-grow">
+                {message}
+            </div>
+            <button onClick={onClose} className="cursor-pointer">
+                <IoClose />
+            </button>
+        </div>
+    );
+};
+
+export default Toast;
