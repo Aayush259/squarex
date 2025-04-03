6f1df9c56b64706d3ece68ff1095f1d7708f4b9f
Aayush259 <aayushk4980@gmail.com>
2025-04-03 22:04:13 +0530

    Add toastReducer to the store for managing toast notifications

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import templateReducer from './templateSlice';
+import toastReducer from './toastSlice';
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         template: templateReducer,
+        toast: toastReducer,
     },
 });
 
